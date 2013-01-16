4b51d8b9b05c7b4c1afe990b4d480ca5a5655488
Per Fagrell <per.fagrell@gmail.com>
2013-01-16 08:34:54 +0100

    Initial setup and package.

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import os
+from setuptools import setup, find_packages
+
+LONG_DESCRIPTION = open(os.path.join(os.path.dirname(__file__), 'README.md')).read()
+
+setup(name="Py-X",
+      version="0.1",
+      keywords="py-x unit test testing unittest unittesting xunit nunit junit xml",
+      author="Per Fagrell",
+      author_email="per.fagrell@gmail.com",
+      license="PSF",
+      classifiers = [
+          'Development Status :: 1 - Pre-alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development'
+          'Topic :: Software Development :: Testing'
+        ],
+      platforms=['All'],
+      long_description=LONG_DESCRIPTION,
+      packages=find_packages())
+
+
